32d87cf4320d8c8bd9f4f5cc5e0c5fabea067f92
vishnu-vardhan56 <105170607+vishnu-vardhan56@users.noreply.github.com>
2023-07-03 23:44:30 +0530

    Added solution - LeetHub

---
+++ Pass by Reference and Value - GFG/pass-by-reference-and-value.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            Solution obj = new Solution();
+            int ans[] = obj.passedBy(n, m);
+
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int[] passedBy(int a, int b) {
+        int arr[] = {a+1,b+2};
+        return arr;
+    }
+}
