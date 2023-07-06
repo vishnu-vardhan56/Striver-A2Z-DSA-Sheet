60f0dea79ec3bc3124c975f9ac3a1e424344a0ec
vishnu-vardhan56 <105170607+vishnu-vardhan56@users.noreply.github.com>
2023-07-06 12:46:59 +0530

    Added solution - LeetHub

---
+++ Count Digits - GFG/count-digits.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.evenlyDivides(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    static int evenlyDivides(int N){
+       int n = N;
+       int c = 0;
+       while(n>0){
+           int rem = n%10;
+           if(rem!=0 && N%rem==0){
+               c+=1;
+           }
+           n/=10;
+       }
+       return c;
+    }
+}
