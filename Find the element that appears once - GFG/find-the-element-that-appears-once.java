e3b20257c3104b8e7a4114f67ebd3a41334443fc
vishnu-vardhan56 <105170607+vishnu-vardhan56@users.noreply.github.com>
2023-07-28 13:19:35 +0530

    Added solution - LeetHub

---
+++ Find the element that appears once - GFG/find-the-element-that-appears-once.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; ++i)
+                arr[i] = sc.nextInt ();
+
+    		System.out.println (new Sol().search (arr, n));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    public static int search(int arr[], int n)
+    {
+        int ans = 0;
+        for(int i:arr){
+            ans^=i;
+        }
+        return ans;
+    }
+}
